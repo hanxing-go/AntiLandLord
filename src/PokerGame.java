24d9f2d1de540137c3930868098f05234e623d42
hanxing_go <1209978808@qq.com>
2024-11-14 16:41:58 +0800

    第一次测试Commit和Push

--- src/PokerGame.java
+++ src/PokerGame.java
@@ -9,6 +9,7 @@ public class PokerGame {
         String[] color = {"♠", "♣", "♥", "♦"};
         String[] number = {"3","4","5", "6", "7", "8", "9", "10","J","Q","K","A","2"};
 
+        System.out.println(color);
     }
     public PokerGame() {
 
