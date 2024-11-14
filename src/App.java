64dc704180666f983bef8551422209fd47175b6b
hanxing_go <1209978808@qq.com>
2024-11-14 16:39:13 +0800

    初始化项目

---
+++ src/App.java
@@ -0,0 +1,11 @@
+public class App {
+    public static void main(String[] args) {
+        /*
+        实际流程:
+        1. 准备牌
+        2. 洗牌
+        3. 发牌
+         */
+        new PokerGame();
+    }
+}
---
+++ src/PokerGame.java
@@ -0,0 +1,17 @@
+public class PokerGame {
+    //准备牌
+    //静态代码块
+    //特点：随着类的加载而加载，并且只执行一次
+    static {
+        //♠，♣，♥，♦
+        //3, 4 , 5, 6, 7, 8, 9, 10, J, Q, K, A, 2
+        //Joker-A Joker-B
+        String[] color = {"♠", "♣", "♥", "♦"};
+        String[] number = {"3","4","5", "6", "7", "8", "9", "10","J","Q","K","A","2"};
+
+    }
+    public PokerGame() {
+
+    };
+
+}
